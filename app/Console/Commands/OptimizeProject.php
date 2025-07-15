da0f4bef4b18296945cbd19fa161dbdfd31886fb
Jaman IT <rikidavidtra.2310@gmail.com>
2025-07-15 12:08:31 +0700

    update

--- app/Console/Commands/OptimizeProject.php
+++ app/Console/Commands/OptimizeProject.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
 
 class OptimizeProject extends Command
 {
@@ -19,7 +18,7 @@ class OptimizeProject extends Command
      *
      * @var string
      */
-    protected $description = 'Clear and optimize common Laravel caches including Filament. Also deletes Laravel logs.';
+    protected $description = 'Clear and optimize common Laravel caches including Filament.';
 
     /**
      * Execute the console command.
@@ -40,15 +39,6 @@ class OptimizeProject extends Command
             $this->call($command);
         }
 
-        $logPath = storage_path('logs');
-        $deleted = 0;
-
-        foreach (File::glob($logPath . '/*.log') as $file) {
-            File::delete($file);
-            $deleted++;
-        }
-
-        $this->info("Deleted $deleted log file(s).");
         $this->info('All done! Laravel project optimized.');
     }
 }
